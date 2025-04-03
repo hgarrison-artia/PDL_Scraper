7a2edc78ee19e28485caab3019ff0685c599d16c
hjg20 <huntergarrison2020@gmail.com>
2025-04-03 09:04:12 -0400

    gui updates

--- AK/AK.py
+++ AK/AK.py
@@ -9,8 +9,10 @@ D = 682.03
 tables = tabula.read_pdf('AK.pdf', area=[A,B,A+D,B+C], pages='all')
 
 df = pd.concat(tables).reset_index(drop=True)
-df.columns = ['Group', 'Market Basket', 'Product', 'Type', 'GNN', 'Strength', 'PDL Status', 'PDL Status Effective Date', 'Status Change from Previous']
+df.columns = ['Group', 'therapeutic_class', 'pdl_name', 'Type', 'GNN', 'Strength', 'status', 'PDL Status Effective Date', 'Status Change from Previous']
 
-print(df)
+df = df[['therapeutic_class', 'pdl_name', 'status']]
+
+df['status'] = ['Preferred' if status == 'ON' else 'Non-Preferred' for status in df['status']]
 
 df.to_csv('AK_PDL.csv', index=False)
