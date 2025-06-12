69e5dc651cbad6a6c6c205f3c209ed64c441bfa9
hgarrison-artia <hgarrison@artiasolutions.com>
2025-06-12 09:26:34 -0400

    Updates to AK
    Altered AK.py to pull strengths along with the drug name.

--- AK/AK.py
+++ AK/AK.py
@@ -4,8 +4,10 @@ df = pd.read_excel('AK.xlsx')
 
 df.columns = ['Group', 'therapeutic_class', 'pdl_name', 'Type', 'GNN', 'Strength', 'status', 'PDL Status Effective Date', 'Status Change from Previous']
 
-df = df[['therapeutic_class', 'pdl_name', 'status']]
+df['pdl_name'] = df['pdl_name'] + ': ' + df['Strength'].astype(str)
 
 df['status'] = ['Preferred' if status == 'ON' else 'Non-Preferred' for status in df['status']]
+
+df = df[['therapeutic_class', 'pdl_name', 'status']]
 
 df.to_csv('AK_PDL.csv', index=False)
