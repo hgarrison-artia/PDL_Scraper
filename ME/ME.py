d54100dfa77834925394ec3d677944d7ac66ca40
hgarrison-artia <hgarrison@artiasolutions.com>
2025-06-26 09:34:51 -0400

    ME Code Creation

---
+++ ME/ME.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('ME.xlsx', 'SSDC-PDL_Maine-with-criteria')
+df = df.iloc[16:].reset_index(drop=True)
+df = df[['CATEGORY', 'PREFERRED DRUGS', 'NON-PREFERRED DRUGS       PA Required']]
+df.columns = ['therapeutic_class', 'Preferred', 'Non-Preferred']
+df = df.dropna(subset=['Preferred', 'Non-Preferred'], how='all')
+
+df['therapeutic_class'] = df['therapeutic_class'].replace(r'^\s*$', np.nan, regex=True)
+df['therapeutic_class'] = df['therapeutic_class'].ffill()
+df = df.melt(id_vars='therapeutic_class', value_vars=['Preferred', 'Non-Preferred'], var_name='status', value_name='pdl_name')
+df = df[['therapeutic_class', 'pdl_name', 'status']]
+df['therapeutic_class'] = df['therapeutic_class'].str.lstrip()
+
+df = df.dropna(subset=['pdl_name'])
+df = df.sort_values('therapeutic_class').reset_index(drop=True)
+
+df['pdl_name'] = (df['pdl_name'].str.replace(r'\s+', ' ', regex=True).str.strip()                            )
+
+
+df.to_csv('ME_PDL.csv', index=False)
+
---
+++ ME/readme.txt
@@ -0,0 +1,3 @@
+1. Go to the ME website and download the PDL as an Excel File
+2. Rename the file to ME.xlsx and put it in this folder
+3. Run ME.py
