42f3251d84d68eb0a57ea1ee36bfadc9fc94f726
sbuchanan-artia <sbuchanan@artiasolutions.com>
2025-07-08 13:36:15 -0400

    NC Updates
    Updates to NC Code and Output

--- NC/NC.py
+++ NC/NC.py
@@ -13,6 +13,8 @@ Subclass = None
 Sub2class = None
 drugs = []
 
+
+
 for row in ws.iter_rows():
     for idx, cell in enumerate(row):
         value = cell.value
@@ -40,7 +42,7 @@ for row in ws.iter_rows():
             drugs.append([Class, Subclass, Sub2class, drug, status])
 
 df = pd.DataFrame(drugs, columns=['therapeutic_class', 'Subclass', 'Sub2class', 'pdl_name', 'status'])
-
+df = df[df['therapeutic_class'].str.strip().str.upper() != 'DIABETIC SUPPLIES']
 # Drop drugs that are identical to any class, subclass, or sub2class
 mask = ~(
     (df['pdl_name'] == df['therapeutic_class']) |
@@ -52,6 +54,7 @@ df = df[mask]
 df = df[~df['pdl_name'].str.lower().isin(['preferred', 'non-preferred'])]
 df['therapeutic_class'] = df['therapeutic_class'].fillna('') + ': ' + df['Subclass'].fillna('') + ': ' + df['Sub2class'].fillna('')
 df = df.drop(['Subclass', 'Sub2class'], axis=1)
+
 df = df[df['pdl_name'].str.len() <= 200]
 
 df.to_csv('NC_PDL.csv', index=False)
