b60faefd25de6a795e847eac806c6f596a51671f
hgarrison-artia <hgarrison@artiasolutions.com>
2025-09-11 09:18:49 -0400

    Updates

---
+++ client_subscription_report.sql
@@ -0,0 +1,46 @@
+select c.id,
+       c.name,
+       c.is_active,
+       number_of_users as user_limit,
+       count(distinct u.id) as user_count,
+       CASE WHEN cpf_bid_analysis.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Bid Analyses",
+       CASE WHEN cpf_calendar.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Calendar",
+       CASE WHEN cpf_notifications.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Daily Alerts",
+       CASE WHEN cpf_drug_info.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Drug Information",
+       CASE WHEN cpf_market_reports.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Market Reports",
+       CASE WHEN cpf_state_reports_premium.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "State Reports (Premium)",
+       CASE WHEN cpf_state_reports.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "State Reports (Standard)",
+       CASE WHEN cpf_calendar_mobile.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Calendar (Mobile)",
+       CASE WHEN cpf_market_share_mobile.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Market Share (Mobile)",
+       CASE WHEN cpf_notifications_mobile.client_id IS NOT NULL THEN TRUE ELSE FALSE END AS "Notifications (Mobile)",
+       count(distinct d.id) as tracked_drug_count,
+       string_agg(distinct d.name, ',') as tracked_drugs,
+       count(distinct c2.id) as tracked_class_count,
+       string_agg(distinct c2.name, ',') as tracked_classes
+from clients c
+    join users u on c.id = u.client_id
+    LEFT JOIN client_premium_features cpf_bid_analysis on c.id = cpf_bid_analysis.client_id AND cpf_bid_analysis.premium_feature_id = 1
+    LEFT JOIN client_premium_features cpf_calendar on c.id = cpf_calendar.client_id AND cpf_calendar.premium_feature_id = 2
+    LEFT JOIN client_premium_features cpf_drug_info on c.id = cpf_drug_info.client_id AND cpf_drug_info.premium_feature_id = 3
+    LEFT JOIN client_premium_features cpf_notifications on c.id = cpf_notifications.client_id AND cpf_notifications.premium_feature_id = 5
+    LEFT JOIN client_premium_features cpf_state_reports on c.id = cpf_state_reports.client_id AND cpf_state_reports.premium_feature_id = 6
+    LEFT JOIN client_premium_features cpf_state_reports_premium on c.id = cpf_state_reports_premium.client_id AND cpf_state_reports_premium.premium_feature_id = 7
+    LEFT JOIN client_premium_features cpf_calendar_mobile on c.id = cpf_calendar_mobile.client_id AND cpf_calendar_mobile.premium_feature_id = 8
+    LEFT JOIN client_premium_features cpf_notifications_mobile on c.id = cpf_notifications_mobile.client_id AND cpf_notifications_mobile.premium_feature_id = 9
+    LEFT JOIN client_premium_features cpf_market_share_mobile on c.id = cpf_market_share_mobile.client_id AND cpf_market_share_mobile.premium_feature_id = 10
+    LEFT JOIN client_premium_features cpf_market_reports on c.id = cpf_market_reports.client_id AND cpf_market_reports.premium_feature_id = 11
+    join client_tracked_drugs ctd on c.id = ctd.client_id
+    join drugs d on ctd.drug_id = d.id
+    join classifications c2 on d.classification_id = c2.id
+group by c.id,
+        cpf_bid_analysis.client_id,
+        cpf_calendar.client_id,
+        cpf_drug_info.client_id,
+        cpf_notifications.client_id,
+        cpf_state_reports.client_id,
+        cpf_state_reports_premium.client_id,
+        cpf_calendar_mobile.client_id,
+        cpf_notifications_mobile.client_id,
+        cpf_market_share_mobile.client_id,
+        cpf_market_reports.client_id
+order by id;
